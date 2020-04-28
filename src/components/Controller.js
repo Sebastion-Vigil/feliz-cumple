d831a0a340715ba789f3c1360a676aa375b9b10e
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-04-28 03:33:21 -0500

    Basic layout for Controller component

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -6,7 +6,28 @@ class Controller extends React.Component {
     render() {
         return (
             <div className='Controller'>
-
+              <div className='button-section'>
+                  <div className='d-pad'>
+                      <div className='d-pad-section'>
+                          <div className='d-square up'></div>
+                      </div>
+                      <div className='d-pad-section'>
+                          <div className='d-square left'></div>
+                          <div className='d-square'></div>
+                          <div className='d-square right'></div>
+                      </div>
+                      <div className='d-pad-section'>
+                          <div className='d-square down'></div>
+                      </div>
+                  </div>
+              </div>
+              <div className='button-section'>
+                  <div className='start'>Start</div>
+              </div>
+              <div className='button-section'>
+                  <div className='action'>B</div>
+                  <div className='action'>A</div>
+              </div>
             </div>
         )
     }
